425e59b2cf039e77c2a19adfe51cfccfeeb72048
tk <kauati.toiti@gmail.com>
2025-03-11 15:59:03 +0000

    read csv file

--- api/src/main/java/smartask/api/controller/SchedulesController.java
+++ api/src/main/java/smartask/api/controller/SchedulesController.java
@@ -1,19 +1,36 @@
 package smartask.api.controller;
 
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import smartask.api.service.SchedulesService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/schedules")
+@RequiredArgsConstructor
 public class SchedulesController {
+
+    @Autowired
+    private  SchedulesService service;
 
     @GetMapping("/generate")
     public ResponseEntity<String> generateNewSchedule(){
         String response = "SEVEN NATION ARMY GOES BRRRRRRRR";
         return   ResponseEntity.ok(response);
-    } 
+    }
+
+
+    @GetMapping("/read")
+    public ResponseEntity<List<String[]>> readex1(){
+        return   ResponseEntity.ok(service.readex1());
+    }
 }
--- api/src/main/java/smartask/api/service/SchedulesService.java
+++ api/src/main/java/smartask/api/service/SchedulesService.java
@@ -1,4 +1,18 @@
 package smartask.api.service;
 
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+import smartask.api.dataAccess.SchedulesRepository;
+
+import java.util.List;
+
+@Service
+@NoArgsConstructor
 public class SchedulesService {
+
+    private final SchedulesRepository repo = new SchedulesRepository();
+
+    public List<String[]> readex1(){
+        return  repo.readex1();
+    }
 }
