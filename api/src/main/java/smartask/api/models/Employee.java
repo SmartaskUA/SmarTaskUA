274d452283cda0815358a0693e55b50c763d128f
tk <kauati.toiti@gmail.com>
2025-03-12 15:00:10 +0000

    Update Employee id

--- api/src/main/java/smartask/api/models/Employee.java
+++ api/src/main/java/smartask/api/models/Employee.java
@@ -1,6 +1,8 @@
 package smartask.api.models;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -14,7 +16,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @RequiredArgsConstructor
 public class Employee {
     @Id
-    private ObjectId id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
     private String name;
     private JsonNode restrictions; // Stores JSON object directly
 
