b3e5396cc443afabfa95caa25daf89d0346eb0c0
tk <kauati.toiti@gmail.com>
2025-03-12 14:57:56 +0000

    aplicaiton update

--- api/src/main/java/smartask/api/ApiApplication.java
+++ api/src/main/java/smartask/api/ApiApplication.java
@@ -12,18 +12,5 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-	//*
-	// @Bean
-	//	CommandLineRunner clearDatabase(MongoTemplate mongoTemplate) {
-	//		return args -> {
-	//			mongoTemplate.getDb().listCollectionNames().forEach(collectionName -> {
-	//				mongoTemplate.dropCollection(collectionName);
-	//				System.out.println("Dropped collection: " + collectionName);
-	//			});
-	//			System.out.println("All collections have been deleted at startup.");
-	//		};
-	//	}
-	// *
-
 
 }
