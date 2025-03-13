75ac3071c2242cf0d308cf4457b5ebb70c9dc97c
tk <kauati.toiti@gmail.com>
2025-03-13 11:09:28 +0000

    Openai documentation

--- api/src/main/java/smartask/api/controllers/EmployeesController.java
+++ api/src/main/java/smartask/api/controllers/EmployeesController.java
@@ -1,5 +1,7 @@
 package smartask.api.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,23 +16,30 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("employee")
+@Tag(name = "Employee Management", description = "Endpoints for managing employees")
 public class EmployeesController {
 
     @Autowired
     private EmployeeService service;
 
+    @Operation(
+            summary = "Get all employees",
+            description = "Retrieves a list of all employees stored in the system."
+    )
     @GetMapping("/all")
-    public ResponseEntity<List<Employee>> getEmps(){
-        return  ResponseEntity.ok(service.getAll());
+    public ResponseEntity<List<Employee>> getEmps() {
+        return ResponseEntity.ok(service.getAll());
     }
 
+    @Operation(
+            summary = "Get an employee by name",
+            description = "Searches for an employee by their name. If found, returns employee details; otherwise, returns a 404 Not Found response."
+    )
     @GetMapping("/{name}")
-    public  ResponseEntity<Optional<Employee>> getEmpByName(@PathVariable String name){
-        if (service.findByName(name).isPresent()){
+    public ResponseEntity<Optional<Employee>> getEmpByName(@PathVariable String name) {
+        if (service.findByName(name).isPresent()) {
             return ResponseEntity.ok(service.findByName(name));
         }
         return ResponseEntity.notFound().build();
     }
-
-
 }
