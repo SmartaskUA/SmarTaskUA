b2044bf6f07fdc41a57d6e96352b495dcf80c4bf
tk <kauati.toiti@gmail.com>
2025-03-19 16:20:25 +0000

    message being sent to the queue

--- api/src/main/java/smartask/api/event/RabbitMqProducer.java
+++ api/src/main/java/smartask/api/event/RabbitMqProducer.java
@@ -1,7 +1,10 @@
 package smartask.api.event;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import smartask.api.models.requests.ScheduleRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class RabbitMqProducer {
@@ -11,6 +14,19 @@ public class RabbitMqProducer {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public void requestScheduleMessage(ScheduleRequest schedule) {
+        try {
+            String jsonMessage = objectMapper.writeValueAsString(schedule);
+            rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, jsonMessage);
+            System.out.println("Sent message: " + jsonMessage);
+        } catch (JsonProcessingException e) {
+            System.err.println("Error converting ScheduleRequest to JSON: " + e.getMessage());
+        }
+    }
 
     public void sendMessage(String message) {
         rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, message);
