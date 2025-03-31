baeef29a132b3f751b440d7fe9c4899ac9f3d468
Vitalie03 <vitalie@ua.pt>
2025-03-31 17:42:13 +0100

    VIsual changes, on sidebar, sidebar_manager and login page

--- frontend/src/components/Sidebar_Manager.jsx
+++ frontend/src/components/Sidebar_Manager.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import { Home, CalendarDays, Users, Briefcase, CircleUserRound, Code2 } from "lucide-react"; 
-import { Link } from "react-router-dom";  
+import { Link, useLocation } from "react-router-dom";  
 import "../styles/Manager.css";
 import logo from '../assets/images/Logo.png';
 
 const Sidebar_Manager = () => {
+  const location = useLocation();
+
+  const isActive = (path) => location.pathname === path;
+
   return (
     <div className="sidebar">
       <Link to="/">
         <img src={logo} alt="SmarTask Logo" className="logo-img" />
       </Link>
       <nav className="nav-links">
-        <Link to="/manager" className="nav-item">
+        <Link to="/manager" className={`nav-item ${isActive("/manager") ? "active" : ""}`}>
           <Home size={20} className="icon" /> Home
         </Link>
-        <Link to="/manager/listcalendar" className="nav-item">
+        <Link to="/manager/listcalendar" className={`nav-item ${isActive("/manager/listcalendar") ? "active" : ""}`}>
           <CalendarDays size={20} className="icon"/> Calendar
         </Link>
-        <Link to="/manager/teams" className="nav-item"> 
+        <Link to="/manager/teams" className={`nav-item ${isActive("/manager/teams") ? "active" : ""}`}> 
           <Users size={20} className="icon"/> Teams
         </Link> 
-        <Link to="/manager/employer" className="nav-item"> 
+        <Link to="/manager/employer" className={`nav-item ${isActive("/manager/employer") ? "active" : ""}`}> 
           <Briefcase size={20} className="icon"/> Employees
         </Link> 
-        <Link to="/manager/compareCalendar" className="nav-item"> 
+        <Link to="/manager/compareCalendar" className={`nav-item ${isActive("/manager/compareCalendar") ? "active" : ""}`}> 
           <Code2 size={20} className="icon"/> Compare Algorithms
         </Link> 
       </nav>
