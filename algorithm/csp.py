2d1c706087cd9ccce32b60d2e1a5732f45a6b935
João <joaomonteiro29@ua.pt>
2025-04-17 19:27:35 +0100

    other domain

--- algorithm/csp.py
+++ algorithm/csp.py
@@ -1,212 +1,208 @@
 import copy
-import time
+import random
 import csv
+import time
+import string
 import os
 import json
-from collections import deque
 from formulation import generate_initial_calendar
 
-class Constraint:
-    def __init__(self, vars):
-        self.vars = vars
-    def propagate(self, domains, csp):
-        """Return True if domains changed, None if no change, or False on failure."""
-        raise NotImplementedError
-
-def revise(domains, xi, xj, func):
-    """Revise domain of xi wrt xj and func(xi_val, xj_val)."""
-    revised = False
-    to_remove = []
-    for a in domains[xi]:
-        if not any(func(a, b) for b in domains[xj]):
-            to_remove.append(a)
-    if to_remove:
-        domains[xi] = [a for a in domains[xi] if a not in to_remove]
-        revised = True
-    return revised
-
-class BinaryConstraint(Constraint):
-    def __init__(self, x, y, func):
-        super().__init__([x, y])
-        self.x, self.y = x, y
-        self.func = func
-
-    def propagate(self, domains, csp):
-        r1 = revise(domains, self.x, self.y, self.func)
-        r2 = revise(domains, self.y, self.x, lambda b,a: self.func(a,b))
-        if r1 is False or r2 is False:
-            return False
-        if r1 or r2:
-            return True
-        return None
-
-class SumAtMost(Constraint):
-    def __init__(self, vars, k):
-        super().__init__(vars)
-        self.k = k
-
-    def propagate(self, domains, csp):
-        lb = 0; ub = 0
-        for v in self.vars:
-            dom = domains[v]
-            if '0' in dom or 'F' in dom:
-                minv = 0
-            else:
-                minv = 1
-            maxv = 1 if any(val in ['A','B'] for val in dom) else 0
-            lb += minv; ub += maxv
-        if lb > self.k:
-            return False
-        if ub <= self.k:
-            return None
-        changed = False
-        if lb == self.k:
-            for v in self.vars:
-                dom = domains[v]
-                if '0' in dom or 'F' in dom:
-                    newdom = [val for val in dom if val not in ['A','B']]
-                    if len(newdom) < len(dom):
-                        domains[v] = newdom
-                        changed = True
-        return True if changed else None
-
-class CountAtLeast(Constraint):
-    def __init__(self, vars, team, k):
-        super().__init__(vars)
-        self.team = team
-        self.k = k
-
-    def propagate(self, domains, csp):
-        ub = 0
-        for v in self.vars:
-            dom = domains[v]
-            if self.team in dom:
-                ub += 1
-        if ub < self.k:
-            return False
-        return None
-
-class No6ConsecutiveWork(Constraint):
-    def __init__(self, emp, num_days):
-        windows = []
-        for start in range(1, num_days - 5):
-            win = []
-            for d in range(start, start + 6):
-                win.extend([f"{emp}_{d}_M", f"{emp}_{d}_T"] )
-            windows.append(win)
-        super().__init__([v for win in windows for v in win])
-        self.windows = windows
-
-    def propagate(self, domains, csp):
-        for win in self.windows:
-            full = True
-            for i in range(0, len(win), 2):
-                dom_m = domains[win[i]]
-                dom_t = domains[win[i+1]]
-                if any(val in ['0','F'] for val in dom_m) or any(val in ['0','F'] for val in dom_t):
-                    full = False
-                    break
-            if full:
-                return False
-        return None
-
 class CSP:
-    def __init__(self, variables, domains):
-        self.variables = list(variables)
-        self.domains = {v:list(domains[v]) for v in variables}
-        self.var_to_constraints = {v: [] for v in variables}
+    def __init__(self, variables, domains, constraints):
+        self.variables = variables
+        self.domains = domains
+        self.constraints = constraints
 
-    def add_constraint(self, constraint):
-        for v in constraint.vars:
-            self.var_to_constraints[v].append(constraint)
-
-    def propagate(self, var):
-        queue = deque([var])
-        while queue:
-            v = queue.popleft()
-            for c in list(self.var_to_constraints[v]):
-                res = c.propagate(self.domains, self)
-                if res is False:
+    def check_constraints(self, var, value, assignment):
+        temp_assignment = assignment.copy()
+        temp_assignment[var] = value
+        for constraint_key, constraint_func in self.constraints.items():
+            if isinstance(constraint_key, tuple):
+                if len(constraint_key) == 2:
+                    v1, v2 = constraint_key
+                    if v1 in temp_assignment and v2 in temp_assignment:
+                        val1 = temp_assignment[v1]
+                        val2 = temp_assignment[v2]
+                        if not constraint_func(val1, val2):
+                            return False
+                else:
+                    values = [temp_assignment.get(v) for v in constraint_key]
+                    if None not in values and not constraint_func(values):
+                        return False
+            else:
+                if not constraint_func(var, temp_assignment):
                     return False
-                if res:
-                    for u in c.vars:
-                        if len(self.domains[u]) == 1 and u not in queue:
-                            queue.append(u)
         return True
 
-    def select_variable(self):
-        choices = [v for v in self.variables if len(self.domains[v]) > 1]
-        if not choices:
+    def propagate(self, domains, var, value):
+        domains[var] = [value]
+        queue = [(var, value)]
+        while queue:
+            curr_var, curr_val = queue.pop(0)
+            for other_var in self.variables:
+                if other_var == curr_var or len(domains[other_var]) == 1:
+                    continue
+                new_domain = []
+                for val in domains[other_var]:
+                    if self.check_constraints(other_var, val, {curr_var: curr_val, other_var: val}):
+                        new_domain.append(val)
+                if not new_domain:
+                    return False
+                domains[other_var] = new_domain
+                if len(new_domain) == 1:
+                    queue.append((other_var, new_domain[0]))
+        return True
+
+    def select_variable(self, domains):
+        unassigned_vars = [v for v in domains if len(domains[v]) > 1]
+        if not unassigned_vars:
             return None
-        return min(choices, key=lambda v: len(self.domains[v]))
+        return min(unassigned_vars, key=lambda var: len(domains[var]))
 
-    def order_values(self, var):
-        dom = self.domains[var]
-        return sorted(dom, key=lambda val: 0 if val in ['A','B'] else 1)
+    def search(self, domains=None, timeout=60):
+        start_time = time.time()
+        if domains is None:
+            domains = copy.deepcopy(self.domains)
 
-    def validate_solution(self, assignment):
-        for v, dom in self.domains.items():
-            if len(dom) != 1:
-                return False
-        assign = {v:self.domains[v][0] for v in self.domains}
-        return True
-
-    def search(self, timeout=60):
-        start = time.time()
-        def backtrack():
-            if time.time() - start > timeout:
+        def timed_search(domains, depth=0):
+            if time.time() - start_time > timeout:
+                print(f"\nExited at the timeout of {timeout} s")
                 return None
-            var = self.select_variable()
+            if any(len(lv) == 0 for lv in domains.values()):
+                return None
+            if all(len(lv) == 1 for lv in domains.values()):
+                assignment = {v: lv[0] for v, lv in domains.items()}
+                return {"assignment": assignment}
+            var = self.select_variable(domains)
             if var is None:
-                return {v:self.domains[v][0] for v in self.domains}
-            orig_dom = list(self.domains[var])
-            for val in self.order_values(var):
-                backup = copy.deepcopy(self.domains)
-                self.domains[var] = [val]
-                if self.propagate(var):
-                    result = backtrack()
-                    if result is not None:
-                        return result
-                self.domains = backup
+                return None
+            values = domains[var].copy()
+            random.shuffle(values)
+            for val in values:
+                new_domains = copy.deepcopy(domains)
+                new_domains[var] = [val]
+                if self.propagate(new_domains, var, val):
+                    solution = timed_search(new_domains, depth + 1)
+                    if solution is not None:
+                        return solution
             return None
-        return backtrack()
 
-def solve_calendar():
-    vars, domains, employees, num_days, num_employees, holidays = generate_initial_calendar()
-    csp = CSP(vars, domains)
+        return timed_search(domains)
 
+def employee_scheduling():
+    tic = time.time()
+    variables, domains, employees, num_days, num_employees, holidays = generate_initial_calendar()
+
+    constraints = {}
+    
     for emp in employees:
         for d in range(1, num_days):
-            v_t = f"{emp}_{d}_T"
-            v_m = f"{emp}_{d+1}_M"
-            csp.add_constraint(BinaryConstraint(v_t, v_m,
-                lambda x,y: not (x in ['A','B'] and y in ['A','B'])))
-    for emp in employees:
-        for d in range(1, num_days+1):
-            v_m = f"{emp}_{d}_M"; v_t = f"{emp}_{d}_T"
-            csp.add_constraint(BinaryConstraint(v_m, v_t,
-                lambda x,y: not (x in ['A','B'] and y in ['A','B'])))
+            var_t = f"{emp}_{d}_T"
+            var_m = f"{emp}_{d+1}_M"
+            constraints[(var_t, var_m)] = lambda t_val, m_val: not (t_val in ["A", "B"] and m_val in ["A", "B"])
+
+    csp = CSP(variables, domains, constraints)
 
     for emp in employees:
-        emp_vars = [f"{emp}_{d}_{s}" for d in range(1, num_days+1) for s in ('M','T')]
-        csp.add_constraint(SumAtMost(emp_vars, 20))
-        holiday_vars = [f"{emp}_{h}_{s}" for h in holidays for s in ('M','T')]
-        csp.add_constraint(SumAtMost(holiday_vars, 2))
-        csp.add_constraint(No6ConsecutiveWork(emp, num_days))
+        emp_vars = [f"{emp}_{d}_{s}" for d in range(1, num_days + 1) for s in ["M", "T"]]
+        for start in range(num_days - 5):
+            window_vars = [f"{emp}_{d}_{s}" for d in range(start + 1, start + 7) for s in ["M", "T"]]
+            handle_ho_constraint(csp, window_vars, lambda values: not all(v in ["A", "B"] for v in values))   
+        handle_ho_constraint(csp, emp_vars, lambda values: sum(1 for v in values if v in ["A", "B"]) <= 40)
+        holiday_vars = [f"{emp}_{d}_{s}" for d in holidays for s in ["M", "T"]]
+        handle_ho_constraint(csp, holiday_vars, lambda values: sum(1 for v in values if v in ["A", "B"]) <= 2)
 
-    for d in range(1, num_days+1):
-        m_vars = [f"{e}_{d}_M" for e in employees]
-        t_vars = [f"{e}_{d}_T" for e in employees]
-        csp.add_constraint(CountAtLeast(m_vars, 'A', 2))
-        csp.add_constraint(CountAtLeast(m_vars, 'B', 1))
-        csp.add_constraint(CountAtLeast(t_vars, 'A', 2))
-        csp.add_constraint(CountAtLeast(t_vars, 'B', 1))
+    for day in range(1, num_days + 1):
+        for shift in ["M", "T"]:
+            day_shift_vars = [f"{emp}_{day}_{shift}" for emp in employees]
+            handle_ho_constraint(csp, day_shift_vars, lambda values: (
+                sum(1 for v in values if v == "A") >= 2 and
+                sum(1 for v in values if v == "B") >= (1 if shift == "T" else 1)
+            ))
 
-    sol = csp.search(timeout=600)
-    if sol:
-        print("Solution found!")
+    solution = csp.search(timeout=600)
+    if solution and "assignment" in solution:
+        assignment = solution["assignment"]
+        generate_calendar(assignment, num_employees, num_days, employees)
+        toc = time.time()
+        print(f"Execution time: {toc - tic:.2f} seconds")
+        analyze_solution(assignment, employees, num_days, holidays)
+        return build_schedule_table(assignment, num_employees, num_days, employees)
     else:
-        print("No solution within timeout.")
+        print("No solution found within timeout or constraints too restrictive.")
+        return None
 
-if __name__ == '__main__':
-    solve_calendar()
+def handle_ho_constraint(csp, variables, constraint_func):
+    def constraint(var, assignment):
+        values = [assignment.get(v, None) for v in variables]
+        if None in values:
+            return True
+        return constraint_func(values)
+    constraint_key = f"multi_{'_'.join(variables)}"
+    csp.constraints[constraint_key] = constraint
+
+def build_schedule_table(assignment, num_employees, num_days, employees):
+    table = []
+    header = ["Employee"] + [str(day) for day in range(1, num_days + 1)]
+    table.append(header)
+    for emp in employees:
+        row = [emp]
+        for d in range(1, num_days + 1):
+            m_val = assignment.get(f"{emp}_{d}_M", "0")
+            t_val = assignment.get(f"{emp}_{d}_T", "0")
+            if m_val == "F" or t_val == "F":
+                shift = "FF"
+            else:
+                shift = f"{m_val if m_val in ['A', 'B'] else '-'}{t_val if t_val in ['A', 'B'] else '-'}"
+            row.append(shift)
+        table.append(row)
+    return table
+
+def generate_calendar(assignment, num_employees, num_days, employees):
+    with open("calendario_turnos.csv", "w", newline="") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["Employee"] + [str(day) for day in range(1, num_days + 1)])
+        for emp in employees:
+            employee_schedule = []
+            m_count = 0
+            t_count = 0
+            for d in range(1, num_days + 1):
+                m_val = assignment.get(f"{emp}_{d}_M", "0")
+                t_val = assignment.get(f"{emp}_{d}_T", "0")
+                if m_val == "F" or t_val == "F":
+                    shift = "FF"
+                else:
+                    shift = f"{m_val if m_val in ['A', 'B'] else '-'}{t_val if t_val in ['A', 'B'] else '-'}"
+                    if m_val in ["A", "B"]:
+                        m_count += 1
+                    if t_val in ["A", "B"]:
+                        t_count += 1
+                employee_schedule.append(shift)
+            csvwriter.writerow([emp] + employee_schedule)
+            print(f"Employee {emp}: {t_count} afternoon shifts (T), {m_count} morning shifts (M)")
+
+def analyze_solution(assignment, employees, num_days, holidays):
+    tm_violations = 0
+    for emp in employees:
+        for d in range(1, num_days):
+            t_val = assignment.get(f"{emp}_{d}_T", "0")
+            m_val = assignment.get(f"{emp}_{d+1}_M", "0")
+            if t_val in ["A", "B"] and m_val in ["A", "B"]:
+                tm_violations += 1
+    print(f"\nNumber of T->M restriction violations: {tm_violations}")
+
+    print("\nShifts per team per day:")
+    for day in range(1, num_days + 1):
+        m_a = sum(1 for emp in employees if assignment.get(f"{emp}_{day}_M", "0") == "A")
+        t_a = sum(1 for emp in employees if assignment.get(f"{emp}_{day}_T", "0") == "A")
+        m_b = sum(1 for emp in employees if assignment.get(f"{emp}_{day}_M", "0") == "B")
+        t_b = sum(1 for emp in employees if assignment.get(f"{emp}_{day}_T", "0") == "B")
+        print(f"Day {day}: M_A={m_a}, T_A={t_a}, M_B={m_b}, T_B={t_b}")
+
+    print("\nWorkdays on holidays per employee:")
+    for emp in employees:
+        holiday_workdays = sum(1 for d in holidays for s in ["M", "T"] if assignment.get(f"{emp}_{d}_{s}", "0") in ["A", "B"])
+        print(f"{emp}: {holiday_workdays} work shifts on holidays")
+
+if __name__ == "__main__":
+    employee_scheduling()
