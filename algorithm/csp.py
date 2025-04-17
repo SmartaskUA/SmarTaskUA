0754c5f5bbec6abad0349fd5d76ff8779612838f
João <joaomonteiro29@ua.pt>
2025-04-17 13:09:15 +0100

    reset latter

--- algorithm/csp.py
+++ algorithm/csp.py
@@ -122,6 +122,31 @@ def handle_ho_constraint(csp, variables, constraint_func):
 #         return constraint_func(values)
 #     csp.constraints[tuple(variables)] = constraint
 
+
+# def handle_ho_constraint(domains,constraints,variables,constraint):
+#     A = "".join(variables)
+#     ho_domains = [domains[v] for v in variables]
+#     A_domain = []
+#     for combo in product(*ho_domains):
+#         if constraint(combo):
+#             A_domain.append(combo)
+#     domains[A] = A_domain
+#     variables.append(A)
+#     for i, v in enumerate(variables):
+#         if v == A:
+#             continue
+#         def make_constraint(index, aux=A, var=v):
+#             def binary_constraint(varA, valA, varV, valV):
+#                 if varA == aux:
+#                     return valA[index] == valV
+#                 if varV == aux:
+#                     return valV[index] == valA
+#                 return True
+#             return binary_constraint
+#         c_fn = make_constraint(i)
+#         constraints[(A,v)] = c_fn
+#         constraints[(v,A)] = c_fn
+
 def solve_calendar():
     tic = time.time()
     print("Loading initial calendar data...")
