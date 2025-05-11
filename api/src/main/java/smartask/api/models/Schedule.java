95c32bf262e0b44e09dabd158aca803c89d6f577
tk <kauati.toiti@gmail.com>
2025-05-11 16:13:27 +0100

    metadata in class entity

--- api/src/main/java/smartask/api/models/Schedule.java
+++ api/src/main/java/smartask/api/models/Schedule.java
@@ -1,6 +1,7 @@
 package smartask.api.models;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
@@ -10,40 +11,28 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.Instant;
 import java.util.List;
 
+import java.util.Map;
+
 @Document(collection = "schedules")
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class Schedule {
+
+
     @Id
     private String id;
-    private Instant timestamp =  Instant.now();
+    private Instant timestamp = Instant.now();
     private String title;
     private String algorithm;
     private List<List<String>> data;
+    private Map<String, Object> metadata;
+
 
     public Schedule(List<List<String>> data, String title, String algorithm) {
         this.data = data;
         this.title = title;
         this.algorithm = algorithm;
-        System.out.println(toString());
     }
-
-    public String getId() {
-        return this.id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return this.title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-
 }
