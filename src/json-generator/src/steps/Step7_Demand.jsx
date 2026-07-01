f4d5764e5089f916307c9344de215a0ce95fc4ea
roldao04 <jroldao04@ua.pt>
2026-07-01 14:43:26 +0100

    clean some bugs

--- src/json-generator/src/steps/Step7_Demand.jsx
+++ src/json-generator/src/steps/Step7_Demand.jsx
@@ -295,7 +295,7 @@ const Step7_Demand = () => {
                       <Chip
                         label={`${demandData.length} entries`}
                         size="small"
-                        color="primary"
+                        color="success"
                         sx={{ height: '20px', fontSize: '0.7rem' }}
                       />
                     )}
